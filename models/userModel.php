10e02ef79dc8a99b7e1afb535a35e046d52fe6f3
halucyn0 <31008706+halucyn0@users.noreply.github.com>
2020-12-22 17:51:35 +0100

    sha256 + salt update

--- models/userModel.php
+++ models/userModel.php
@@ -6,7 +6,9 @@ class userModel extends Model{
     public function check($name,$pass) {
         $data=$this->select("SELECT * FROM `users` WHERE `user_login`='".$name."'");
         if(count($data)>0){
-            if($data[0]['user_password']==md5($pass)) return $data[0];
+            $dbPassword = $data[0]['user_password'];
+            $dbSalt = $data[0]['user_salt'];
+            if($dbPassword==hash('sha256', $dbSalt.$pass)) return $data[0];
             else return "";
         }
         else return "";
