356d894f78eda11484ee0272598c465aed837d14
Vergiis <zelazowski00@gmail.com>
2021-01-09 16:31:35 +0100

    Forms

--- templates/main.php
+++ templates/main.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="templates/styles/style_logowania.css">
-<link rel="stylesheet" href="templates/styles/style_topMenu.css">
-<link rel="stylesheet" href="templates/styles/style_Forms.css">
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <title>M&M - Mini-Company Manager</title>
+    <link rel="stylesheet" href="templates/styles/style_logowania.css">
+    <link rel="stylesheet" href="templates/styles/style_topMenu.css">
+    <link rel="stylesheet" href="templates/styles/style_Forms.css">
+    <link rel="shortcut icon" href="templates/res/favicon.jpg" />
 </head>
 <body>
     <?php require_once 'topMenu.php'; ?>
